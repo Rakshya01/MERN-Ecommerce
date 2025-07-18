bd0d8a98acba6d732d5cf04762dea373146274bc
rakshya01 <kunwarrakshya20@gmail.com>
2025-07-18 07:26:59 +0545

    mongodb setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/blissful_bazaar');
+    console.log('Connecting to:', process.env.MONGO_URI); // Debug line
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import path from 'path';
-import express from 'express';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import path from 'path';
+dotenv.config({path:path.resolve('backend/.env')});
+import express from 'express';
 import connectDB from './config/db.js';
+connectDB();
 import cookieParser from 'cookie-parser';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,8 +12,6 @@ import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
-
-connectDB();
 
 const app = express();
 
